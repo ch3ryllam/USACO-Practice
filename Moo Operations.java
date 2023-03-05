0da8de7880f52e7f385ec8e3d67b06e1723d0dd9
ch3ryllam <124017960+ch3ryllam@users.noreply.github.com>
2023-03-05 14:28:27 -0800

    Add files via upload

---
+++ Moo Operations.java
@@ -0,0 +1,26 @@
+//USACO 2023 January Contest, Bronze
+//Problem 3. Moo Operations
+
+import java.util.*;
+public class Moo Operations {
+  public static void main(String[] args){
+    Scanner in = new Scanner(System.in);
+    int n = in.nextInt();
+    String[] text = new String[n];
+    for (int i=0; i<n; i++) {
+      text[i] = in.next();
+    }
+    for (int i=0; i<n; i++) {
+      String str = text[i];
+      if (str.contains("MOO")) {
+        System.out.println(str.length()-3);
+      } else if (str.contains("MOM") || str.contains("OOO")) {
+        System.out.println(str.length()-2);
+      } else if (str.contains("OOM")) {
+        System.out.println(str.length()-1);
+      } else {
+        System.out.println(-1);
+      }
+    }
+  }
+}
