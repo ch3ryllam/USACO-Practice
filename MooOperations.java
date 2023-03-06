a5539118208d11b52fa430512c5077ce90347c77
ch3ryllam <124017960+ch3ryllam@users.noreply.github.com>
2023-03-05 19:46:23 -0800

    Update and rename Moo Operations.java to MooOperations.java

--- MooOperations.java
+++ MooOperations.java
@@ -2,7 +2,7 @@
 //Problem 3. Moo Operations
 
 import java.util.*;
-public class Moo Operations {
+public class MooOperations {
   public static void main(String[] args){
     Scanner in = new Scanner(System.in);
     int n = in.nextInt();
